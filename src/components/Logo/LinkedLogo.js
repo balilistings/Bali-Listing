66f84de45a9a525dcc905be3b3606cf23c0cd45f
tian_1800 <tian.sudibyo@gmail.com>
2025-08-29 14:15:42 +0800

    Use local svg for topbar icon

--- src/components/Logo/LinkedLogo.js
+++ src/components/Logo/LinkedLogo.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
+import img from '../../assets/bali-listing-logo.svg';
 
-import { ExternalLink, Logo, NamedLink } from '../../components';
+import { ExternalLink, NamedLink } from '../../components';
+import { useConfiguration } from '../../context/configurationContext';
 
+import logoCss from '../Logo/Logo.module.css';
 import css from './LinkedLogo.module.css';
+
+// Height constants matching Logo component
+const HEIGHT_24 = 24;
+const HEIGHT_36 = 36;
+const HEIGHT_48 = 48;
+const HEIGHT_OPTIONS = [HEIGHT_24, HEIGHT_36, HEIGHT_48];
+
+// Function to get height class name, matching Logo component logic
+const getHeightClassName = height => {
+  return height === HEIGHT_48
+    ? logoCss.logo48
+    : height === HEIGHT_36
+    ? logoCss.logo36
+    : logoCss.logo24;
+};
 
 /**
  * This component returns a clickable logo.
@@ -31,25 +49,30 @@ const LinkedLogo = props => {
     alt,
     ...rest
   } = props;
+
+  // Get the height of the logo based on the configuration
+  const config = useConfiguration();
+  const { logoSettings } = config.branding;
+  const hasValidLogoSettings =
+    logoSettings?.format === 'image' && HEIGHT_OPTIONS.includes(logoSettings?.height);
+  const logoHeight = hasValidLogoSettings ? logoSettings.height : HEIGHT_24;
+  const heightClassName = getHeightClassName(logoHeight);
+
+  const logoImageClasses = classNames(logoCss.logo, heightClassName, logoImageClassName);
+
   const classes = classNames(rootClassName || css.root, className);
   // Note: href might come as an empty string (falsy), in which case we default to 'LandingPage'.
   return linkToExternalSite?.href ? (
     <ExternalLink className={classes} href={linkToExternalSite.href} target="_self" {...rest}>
-      <Logo
-        layout={layout}
-        className={logoClassName}
-        logoImageClassName={logoImageClassName}
-        alt={alt}
-      />
+      <div className={logoClassName}>
+        <img className={logoImageClasses} src={img} alt={alt || config.marketplaceName} />
+      </div>
     </ExternalLink>
   ) : (
     <NamedLink className={classes} name="LandingPage" {...rest}>
-      <Logo
-        layout={layout}
-        className={logoClassName}
-        logoImageClassName={logoImageClassName}
-        alt={alt}
-      />
+      <div className={logoCss.root}>
+        <img className={logoImageClasses} src={img} alt={alt || config.marketplaceName} />
+      </div>
     </NamedLink>
   );
 };
