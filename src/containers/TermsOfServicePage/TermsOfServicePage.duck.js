a93fed1959fa6335b00ed661340e8b59981da0bd
tian_1800 <tian.sudibyo@gmail.com>
2025-09-24 10:37:30 +0800

    Add locale to sharetribe asset retrieval

--- src/containers/TermsOfServicePage/TermsOfServicePage.duck.js
+++ src/containers/TermsOfServicePage/TermsOfServicePage.duck.js
@@ -1,7 +1,15 @@
 import { fetchPageAssets } from '../../ducks/hostedAssets.duck';
+import { constructLocalizedPageAssets } from '../../util/localeAssetUtils';
+
 export const ASSET_NAME = 'terms-of-service';
 
-export const loadData = (params, search) => dispatch => {
-  const pageAsset = { termsOfService: `content/pages/${ASSET_NAME}.json` };
+export const loadData = (params, search, config, match) => dispatch => {
+  const assetMap = {
+    termsOfService: ASSET_NAME,
+  };
+
+  const pageAsset = constructLocalizedPageAssets(assetMap, match);
+  // throw new Error(JSON.stringify(pageAsset));
+  
   return dispatch(fetchPageAssets(pageAsset, true));
 };
