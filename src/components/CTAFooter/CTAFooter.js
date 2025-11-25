9113f628db28a233f7e2e4a3430c8db4adb1d360
tian_1800 <tian.sudibyo@gmail.com>
2025-11-25 15:49:17 +0800

    Adjust css in about us

--- src/components/CTAFooter/CTAFooter.js
+++ src/components/CTAFooter/CTAFooter.js
@@ -14,32 +14,34 @@ const CTA = ({ variant = 'primary' }) => {
   return (
     <div className={css.container}>
       <div className={css.root}>
-        <div className={css.circleLineLeft} />
-        <div className={css.circleLineRight} />
-        <img src={sofaSrc} className={css.sofa} alt="cta-sofa" fetchpriority="low" />
+        <div className={css.inner}>
+          <div className={css.circleLineLeft} />
+          <div className={css.circleLineRight} />
+          <img src={sofaSrc} className={css.sofa} alt="cta-sofa" fetchpriority="low" />
 
-        <div className={css.content}>
-          <div className={css.textContent}>
-            <h1 className={css.title}>
-              <FormattedMessage
-                id={title}
-                defaultMessage="Ready to list your villa or find your next home"
-              />
-            </h1>
-            <p className={css.subtitle}>
-              <FormattedMessage
-                id={subtitle}
-                defaultMessage="We're building the future of real estate in Bali"
-              />
-            </p>
-            {variant === 'primary' && (
-              <p className={css.tagline}>
-                <FormattedMessage id={tagline} />
+          <div className={css.content}>
+            <div className={css.textContent}>
+              <h1 className={css.title}>
+                <FormattedMessage
+                  id={title}
+                  defaultMessage="Ready to list your villa or find your next home"
+                />
+              </h1>
+              <p className={css.subtitle}>
+                <FormattedMessage
+                  id={subtitle}
+                  defaultMessage="We're building the future of real estate in Bali"
+                />
               </p>
-            )}
-            <Button className={css.button}>
-              <FormattedMessage id={startButton} defaultMessage="Start now" />
-            </Button>
+              {variant === 'primary' && (
+                <p className={css.tagline}>
+                  <FormattedMessage id={tagline} />
+                </p>
+              )}
+              <Button className={css.button}>
+                <FormattedMessage id={startButton} defaultMessage="Start now" />
+              </Button>
+            </div>
           </div>
         </div>
       </div>
